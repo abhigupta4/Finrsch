6ff6f8588c56aae55519c8fd9cd33bb0209dfaae
Abhinav <abhinavgupta6245@gmail.com>
2017-07-06 14:27:29 +0530

    Modified two files

--- Edgar-new-codes/newdb.py
+++ Edgar-new-codes/newdb.py
@@ -3,7 +3,7 @@ import pymysql as PyMySQL
 db = PyMySQL.connect("localhost","arafin_test","&gR^Fh-?)Tn(","arafin_edgar", charset="utf8" )
 cursor = db.cursor()
 
-def add_to_db(tablename,url,q,t1,t2,t3,date,exclink):
+def add_to_db(tablename,url,q,t1,t2,t3,date,exclink,tp,yr):
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS `%s`(
     id int auto_increment not null primary key,
@@ -13,12 +13,12 @@ def add_to_db(tablename,url,q,t1,t2,t3,date,exclink):
     S_Text LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     T_Text LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     F_date VARCHAR(255),
-    Financials varchar(255)
+    Financials varchar(255),
+    F_Type varchar(255),
+    Year varchar(255)
     )""" % tablename)
     db.commit()
 
-    cursor.execute("""insert into `%s` (Link,Quarter,U_Text,S_Text,T_Text,F_date,Financials) values ("%s","%s","%s","%s","%s","%s","%s")""" %
-                   (tablename,url,q,db.escape_string(t1),db.escape_string(t2),db.escape_string(t3),date,exclink))
+    cursor.execute("""insert into `%s` (Link,Quarter,U_Text,S_Text,T_Text,F_date,Financials,F_Type,Year) values ("%s","%s","%s","%s","%s","%s","%s","%s","%s")""" %
+                   (tablename,url,q,t1,t2,t3,date,exclink,tp,yr))
     db.commit()
-
-
